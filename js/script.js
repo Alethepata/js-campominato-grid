e303dbd85db4cd9dcc6995884fbdb0e382c1ecde
Alethepata <alessia.patane@gmail.com>
2023-09-18 15:57:20 +0200

    [Add] for / function square

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('ciao');
+const container = document.querySelector('.container');
+
+for( let i = 1; i <= 100; i++){
+    const square = createSquare(i);
+
+    container.append(square);
+}
+
+function createSquare(index){
+    const squares = document.createElement('div');
+    squares.className = 'square';
+    squares.innerHTML = `<span>${index}</span>`;
+    return squares;
+}
